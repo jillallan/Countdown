c4a562a95a28ed14a139506daa3a1b16787650e9
Jill Allan <54241079+jillallan@users.noreply.github.com>
2023-10-06 10:19:11 +0100

    Add CountDownModel container and conformance to ViewModifierProtocol

---
+++ Countdown/CountdownModelContainer.swift
@@ -0,0 +1,29 @@
+//
+//  CountdownModelContainer.swift
+//  Countdown
+//
+//  Created by Jill Allan on 06/10/2023.
+//
+
+import SwiftData
+import SwiftUI
+
+struct CountdownModelContainer {
+    let container: ModelContainer
+    
+    init(inMemory: Bool) {
+        do {
+            let configuration = ModelConfiguration(isStoredInMemoryOnly: inMemory)
+            container = try ModelContainer(for: Event.self, configurations: configuration)
+        } catch {
+            fatalError("Unable to load model container: \(error.localizedDescription)")
+        }
+    }
+}
+
+extension CountdownModelContainer: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .modelContainer(container)
+    }
+}
