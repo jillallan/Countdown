2b8a32631aa74398efb3fad28463008c7ef0729c
Jill Allan <54241079+jillallan@users.noreply.github.com>
2023-10-06 12:46:38 +0100

    Add ModelPreview for previewing sample data

---
+++ Countdown/EventRow.swift
@@ -0,0 +1,18 @@
+//
+//  EventRow.swift
+//  Countdown
+//
+//  Created by Jill Allan on 06/10/2023.
+//
+
+import SwiftUI
+
+struct EventRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EventRow()
+}
